d9d5fd4b7958cc85899f8084488aebf61b142eef
Boris Sinyapkin <b.sinyapkin@picodata.io>
2025-03-03 19:19:06 +0300

    Introduce custom plugin configuration
    This change adds an ability to spin up test cluster with supplied
    plugin configuration. It adds new optional attribute to #[picotest(...)] macro
    called "config_path".
    
    If it's specified, then original manifest.yaml will be substituted by passed
    services configurations found in plugin config. In such a run, original manifest
    will be backed-up on the disk and restored when cluster is terminated.
    Otherwise, original manifest will be used.
    
    Example:
    
    Below example will spin up cluster with custom configuration located
    in /tmp/plugin_config.yaml
    
    ```
    \#[picotest(config_path = "/tmp/plugin_config.yaml")]
    fn test_some_test() {}
    ```

--- picotest/tests/run.rs
+++ picotest/tests/run.rs
@@ -4,11 +4,22 @@ use constcat::concat;
 use helpers::{build_plugin, wait_for_proc};
 use picotest::*;
 use rstest::*;
-use std::{fs, time::Duration};
+use std::{fs, path::PathBuf, time::Duration};
 
 pub const TMP_DIR: &str = "../tmp/";
 pub const PLUGIN_NAME: &str = "test_plugin";
 pub const PLUGIN_DIR: &str = concat!(TMP_DIR, PLUGIN_NAME);
+pub const PLUGIN_VERSION: &str = "0.1.0";
+pub const PLUGIN_SERVICE_NAME: &str = "main";
+pub const PLUGIN_MANIFEST_FILENAME: &str = "manifest.yaml";
+pub const PLUGIN_MANIFEST_FILENAME_BACKUP: &str = "manifest.backup.yaml";
+pub const PLUGIN_MANIFEST_DIR: &str = concat!(
+    PLUGIN_DIR,
+    "/target/debug/",
+    PLUGIN_NAME,
+    "/",
+    PLUGIN_VERSION
+);
 
 #[derive(Debug)]
 struct Plugin {
@@ -28,6 +39,16 @@ pub fn plugin() -> Plugin {
     }
 }
 
+#[fixture]
+pub fn manifest_path() -> PathBuf {
+    PathBuf::from(PLUGIN_MANIFEST_DIR).join(PLUGIN_MANIFEST_FILENAME)
+}
+
+#[fixture]
+pub fn manifest_backup_path() -> PathBuf {
+    PathBuf::from(PLUGIN_MANIFEST_DIR).join(PLUGIN_MANIFEST_FILENAME_BACKUP)
+}
+
 #[picotest(path = "../tmp/test_plugin")]
 fn test_func_install_plugin(plugin: &Plugin) {
     let enabled = cluster.run_query(format!(
@@ -36,6 +57,50 @@ fn test_func_install_plugin(plugin: &Plugin) {
     ));
     assert!(enabled.is_ok());
     assert!(enabled.is_ok_and(|enabled| enabled.contains("true")));
+}
+
+#[picotest(
+    path = "../tmp/test_plugin",
+    config_path = "./tests/resources/test_plugin_config.yaml"
+)]
+fn test_cusom_plugin_configuration(plugin: &Plugin) {
+    let service_properties = cluster
+        .run_query(format!(
+            r#"SELECT key, value FROM _pico_plugin_config WHERE plugin = '{}' AND entity = '{}';"#,
+            plugin.name, PLUGIN_SERVICE_NAME
+        ))
+        .expect("Failed to run query");
+
+    // TODO: more fine grained verification of key-value pair.
+    assert!(service_properties.contains("should_be_overridden_in_test"));
+}
+
+#[rstest]
+fn test_backup_manifest(manifest_path: PathBuf, manifest_backup_path: PathBuf) {
+    // Test verifies that replacement of plugin configuration leads to
+    // creation of backup manifest file. Original manifest should be
+    // restored from backup when cluster is dropped.
+    let plugin_path = "../tmp/test_plugin";
+    let config_path = "./tests/resources/test_plugin_config.yaml";
+
+    let original_manifest =
+        fs::read_to_string(&manifest_path).expect("Failed to read original manifest");
+    {
+        let _cluster = picotest::run_cluster(plugin_path, Some(config_path), 0);
+
+        assert!(
+            fs::metadata(&manifest_backup_path).is_ok(),
+            "manifest backup should be created"
+        );
+
+        // Cluster is droping. Original manifest should be
+        // restored right after cluster is droped.
+    }
+    let restored_manifest =
+        fs::read_to_string(&manifest_path).expect("Failed to read restored manifest");
+
+    assert_eq!(original_manifest, restored_manifest);
+    assert!(fs::metadata(manifest_backup_path).is_err());
 }
 
 #[picotest(path = "../tmp/test_plugin")]
--- picotest_helpers/src/lib.rs
+++ picotest_helpers/src/lib.rs
@@ -1,9 +1,10 @@
 use anyhow::Context;
-use log::{debug, info, warn};
+use log::{debug, info, trace, warn};
 use pike::cluster::{PicodataInstance, RunParamsBuilder, StopParamsBuilder, Topology};
 use rand::distributions::Alphanumeric;
 use rand::Rng;
 use std::ffi::OsStr;
+use std::fmt::Debug;
 use std::fs;
 use std::io::{BufRead, BufReader, Write};
 use std::path::{Path, PathBuf};
@@ -15,8 +16,17 @@ use std::{
 };
 use uuid::Uuid;
 
+#[cfg(test)]
+mod tests;
+
 const SOCKET_PATH: &str = "cluster/i1/admin.sock";
 const TOPOLOGY_FILENAME: &str = "topology.toml";
+const TARGET_DIR: &str = "target/debug";
+
+const MANIFEST_FILENAME: &str = "manifest.yaml";
+const MANIFEST_FILENAME_BACKUP: &str = "manifest.backup.yaml";
+const MANIFEST_SERVICES: &str = "services";
+const MANIFEST_SERVICE_NAME: &str = "name";
 
 fn parse_topology(path: &PathBuf) -> anyhow::Result<Topology> {
     toml::from_str(
@@ -28,11 +38,16 @@ fn parse_topology(path: &PathBuf) -> anyhow::Result<Topology> {
     ))
 }
 
+fn parse_yaml<T: AsRef<Path>>(path: &T) -> anyhow::Result<serde_yaml::Value> {
+    let reader = fs::File::open(path).context("Failed to open YAML file")?;
+    serde_yaml::from_reader(reader).context("Failed to parse YAML content")
+}
+
 pub struct Cluster {
     pub uuid: Uuid,
     pub plugin_path: PathBuf,
     pub data_dir: PathBuf,
-    pub timeout: u8,
+    pub timeout: Duration,
     socket_path: PathBuf,
     topology: Topology,
     instances: Option<Vec<PicodataInstance>>,
@@ -40,6 +55,12 @@ pub struct Cluster {
 
 impl Drop for Cluster {
     fn drop(&mut self) {
+        if let Err(err) = self.restore_manifest_file() {
+            warn!("Failed to restore plugin manifest file: {err}");
+        }
+        if self.instances.is_none() {
+            return;
+        }
         if let Err(err) = self.stop() {
             warn!("Failed to stop picodata cluster: {err}");
         }
@@ -47,7 +68,8 @@ impl Drop for Cluster {
 }
 
 impl Cluster {
-    pub fn new(plugin_path: &str, data_dir: &str, timeout: u8) -> anyhow::Result<Self> {
+    pub fn new(plugin_path: &str, data_dir: &str, timeout: Duration) -> anyhow::Result<Self> {
+        let plugin_path = PathBuf::from(plugin_path);
         let data_dir = PathBuf::from(data_dir);
         let plugin_path = PathBuf::from(plugin_path);
         let socket_path = plugin_path.join(&data_dir).join(SOCKET_PATH);
@@ -89,6 +111,7 @@ impl Cluster {
             .plugin_path(self.plugin_path.clone())
             .data_dir(self.data_dir.clone())
             .topology(self.topology.clone())
+            .use_release(false)
             .build()?;
 
         debug!("Starting the cluster with parameters {params:?}");
@@ -149,7 +172,7 @@ impl Cluster {
 
             picodata_admin.kill().unwrap();
             if can_connect && plugin_ready {
-                thread::sleep(Duration::from_secs(self.timeout.into()));
+                thread::sleep(self.timeout);
                 return Ok(self);
             }
 
@@ -211,11 +234,88 @@ impl Cluster {
             }
         }
     }
+
+    fn plugin_manifest_dir(&self) -> PathBuf {
+        let (plugin_name, plugin_info) = self
+            .topology
+            .plugins
+            .first_key_value()
+            .expect("Topology should define at least 1 plugin");
+
+        // Use 0.1.0 as default plugin version unless specified in topology.
+        let plugin_version = plugin_info.version.as_ref().map_or("0.1.0", |v| v);
+
+        self.plugin_path
+            .join(TARGET_DIR)
+            .join(plugin_name)
+            .join(plugin_version)
+    }
+
+    fn plugin_manifest_path(&self) -> PathBuf {
+        self.plugin_manifest_dir().join(MANIFEST_FILENAME)
+    }
+
+    fn plugin_manifest_backup_path(&self) -> PathBuf {
+        self.plugin_manifest_dir().join(MANIFEST_FILENAME_BACKUP)
+    }
+
+    fn backup_manifest_file(&self) -> anyhow::Result<()> {
+        let manifest_path = self.plugin_manifest_path();
+        let manifest_backup_path = self.plugin_manifest_backup_path();
+
+        std::fs::copy(manifest_path, manifest_backup_path)
+            .context("Failed to back up plugin manifest file")?;
+
+        Ok(())
+    }
+
+    fn restore_manifest_file(&self) -> anyhow::Result<()> {
+        let manifest_path = self.plugin_manifest_path();
+        let manifest_backup_path = self.plugin_manifest_backup_path();
+
+        if fs::metadata(&manifest_backup_path).is_ok() {
+            std::fs::copy(&manifest_backup_path, &manifest_path)
+                .context("Failed to restore plugin manifest file from backup")?;
+
+            std::fs::remove_file(manifest_backup_path)
+                .context("Failed to remove backup manifest file")?;
+        }
+
+        Ok(())
+    }
 }
 
-pub fn run_cluster(plugin_path: &str, timeout: u8) -> anyhow::Result<Cluster> {
+/// Spin up Picodata cluster
+///
+/// # Arguments
+///
+/// - `plugin_path` - path to the plugin directory.
+/// - `plugin_config_path` - path to the plugin configuration.
+///                          If `None`, default plugin configuration is used.
+/// - `timeout_secs` - amount of seconds to be waited right after cluster is started.
+///
+/// # Returns
+///
+/// A `Result` containing either up and running `Cluster` or instance of [`anyhow::Error`]
+///
+pub fn run_cluster(
+    plugin_path: &str,
+    plugin_config_path: Option<&str>,
+    timeout_secs: u64,
+) -> anyhow::Result<Cluster> {
     let data_dir = tmp_dir();
+    let timeout = Duration::from_secs(timeout_secs);
     let cluster = Cluster::new(plugin_path, &data_dir, timeout)?;
+
+    if let Some(plugin_config_path) = plugin_config_path {
+        cluster
+            .backup_manifest_file()
+            .context("Failed to create backup manifest file")?;
+
+        apply_plugin_configuration(plugin_config_path, cluster.plugin_manifest_path())
+            .context("Failed to apply plugin configuration")?;
+    }
+
     cluster.run()
 }
 
@@ -240,4 +340,83 @@ pub fn tmp_dir() -> String {
             .map(char::from)
             .collect::<String>()
     )
+}
+
+/// Applies input plugin configuration to the manifest file.
+///
+/// # Arguments
+///
+/// - `plugin_config_path` - path to the YAML plugin configuration to be applied.
+/// - `manifest_path` - path to the plugin manifest YAML file.
+///
+fn apply_plugin_configuration<P, M>(plugin_config_path: P, manifest_path: M) -> anyhow::Result<()>
+where
+    P: AsRef<Path> + Debug,
+    M: AsRef<Path> + Debug,
+{
+    info!(
+        "Applying plugin configuration from {:?} to manifest {:?}",
+        plugin_config_path, manifest_path
+    );
+
+    let plugin_config = parse_yaml(&plugin_config_path)?;
+    let mut plugin_manifest = parse_yaml(&manifest_path)?;
+
+    replace_services_configuration(&plugin_config, &mut plugin_manifest)
+        .context("Failed to replace plugin services configuration(s)")?;
+
+    let writer = fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(manifest_path)
+        .expect("Failed to open plugin manifest to write");
+
+    serde_yaml::to_writer(writer, &plugin_manifest).context("Failed to write plugin manifest")
+}
+
+/// Iterate over services in manifest yaml and replace their default configuration
+/// with corresponding config found in provided plugin configuration.
+///
+/// Changes is applied in-place, so the result will be in `plugin_manifest` argument.
+///
+/// # Arguments
+///
+/// - `plugin_config` - yaml-formatted plugin configuration to be applied.
+/// - `plugin_manifest` - yaml-formatted manifest of the plugin.
+///
+fn replace_services_configuration(
+    plugin_config: &serde_yaml::Value,
+    plugin_manifest: &mut serde_yaml::Value,
+) -> anyhow::Result<()> {
+    let manifest_services = plugin_manifest
+        .get_mut(MANIFEST_SERVICES)
+        .context("Failed to get services mapping from plugin manifest")?;
+
+    for service in manifest_services
+        .as_sequence_mut()
+        .expect("Should be a sequence of services")
+    {
+        let service_name = service
+            .get(MANIFEST_SERVICE_NAME)
+            .context("Failed to get name of the service from plugin manifest")?;
+
+        let Some(service_configuration) = plugin_config.get(service_name) else {
+            debug!("Service {service_name:?} was not found in provided plugin configuration");
+            continue;
+        };
+
+        trace!(
+            "Replacing configuration of service {:?} to {:?}",
+            service_name,
+            service_configuration
+        );
+
+        let service_default_configuration = service
+            .get_mut("default_configuration")
+            .context("Failed to get service default configuration from plugin manifest")?;
+
+        *service_default_configuration = service_configuration.clone();
+    }
+
+    Ok(())
 }
